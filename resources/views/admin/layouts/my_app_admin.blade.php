62b03000f9afde01de64b68d0252980a07b3913b
Helen Sakovych <helen40931@gmail.com>
2018-05-16 19:59:24 +0300

    create category unsuccesful

--- resources/views/admin/layouts/my_app_admin.blade.php
+++ resources/views/admin/layouts/my_app_admin.blade.php
@@ -51,7 +51,7 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expended="false">Блог</a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Категории</a></li>
+                            <li><a href="#{{--{{route('admin.category.index')}}--}}">Категории</a></li>
                             <li><a href="#">Посты</a></li>
                             <li><a href="#">Теги</a></li>
                         </ul>
